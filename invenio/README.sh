75cc2940c4a57b9cf6cc4ac5b2266b4176accf5d
Rémi Ducceschi <remi.ducceschi@gmail.com>
2016-10-19 11:09:33 +0200

    Update README.sh

--- invenio/README.sh
+++ invenio/README.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-echo "lol"
+# Run me to launch the example application:
+# ./README.sh
+
+# First we start elastisearch. If you don't have it, please install it.
+elasticsearch -d
+# Then we run rabbitmq. If you don't have it, please install it.
+rabbitmq-server -d
+# We setup the application.
+./app-setup.sh
+# Let's put some records in the database.
+./app-fixtures.sh
+# And now we run it. We need to export FLASK_APP variable so flask knows which file to run.
+FLASK_APP=app.py flask run
